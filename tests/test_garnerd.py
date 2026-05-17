c4697d7e9c7d0a8e865c83004faa058517283c49
Gary Stump <gstumpjr@gmail.com>
2026-05-17 15:28:58 -0400

    feat: use temporary directory in test

--- tests/test_garnerd.py
+++ tests/test_garnerd.py
@@ -1,6 +1,7 @@
 import unittest
 from pathlib import Path
 from src.garnerd.filestore.directory import DirectoryFileStore
+from tempfile import TemporaryDirectory
 
 class TestGarnerd(unittest.TestCase):
     
@@ -19,9 +20,10 @@ class TestGarnerd(unittest.TestCase):
         return super().tearDownClass()
     
     def testFileStore_file_path(self):
-        fs = DirectoryFileStore('/', dir_depth=6)
-        t: Path = fs.file_path(path_key='56bb3d0a2a7f294967f02dbc2de2a403ae3ba98b124d840273a6e46e081cf67c', file_size=123)
-        self.assertEqual(str(t.name), '0a2a7f294967f02dbc2de2a403ae3ba98b124d840273a6e46e081cf67c.3r')
-        self.assertEqual(str(t.parent), '/5/6/b/b/3/d')
-        self.assertEqual(str(t), '/5/6/b/b/3/d/0a2a7f294967f02dbc2de2a403ae3ba98b124d840273a6e46e081cf67c.3r')
+        with TemporaryDirectory() as tdir:
+            fs = DirectoryFileStore(tdir, dir_depth=6)
+            t: Path = fs.file_path(path_key='56bb3d0a2a7f294967f02dbc2de2a403ae3ba98b124d840273a6e46e081cf67c', file_size=123)
+            self.assertEqual(str(t.name), '0a2a7f294967f02dbc2de2a403ae3ba98b124d840273a6e46e081cf67c.3r')
+            self.assertEqual(str(t.parent), f'{tdir}/5/6/b/b/3/d')
+            self.assertEqual(str(t), f'{tdir}/5/6/b/b/3/d/0a2a7f294967f02dbc2de2a403ae3ba98b124d840273a6e46e081cf67c.3r')
         
